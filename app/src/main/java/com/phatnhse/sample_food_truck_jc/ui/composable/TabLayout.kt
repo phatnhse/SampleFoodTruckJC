c36cc502e27f30578fac1078743da402ce70a177
phatnhse <phatnhse@gmail.com>
2023-05-05 23:24:12 +0700

    Add tab layout animation

--- app/src/main/java/com/phatnhse/sample_food_truck_jc/ui/composable/TabLayout.kt
+++ app/src/main/java/com/phatnhse/sample_food_truck_jc/ui/composable/TabLayout.kt
@@ -1,8 +1,14 @@
 package com.phatnhse.sample_food_truck_jc.ui.composable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.util.Log
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Tab
@@ -10,17 +16,26 @@ import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.phatnhse.sample_food_truck_jc.ui.theme.PaddingNormal
+import com.phatnhse.sample_food_truck_jc.ui.theme.PaddingSmall
 import com.phatnhse.sample_food_truck_jc.ui.theme.RoundedLarge
 import com.phatnhse.sample_food_truck_jc.ui.theme.onBackgroundSecondary
 import com.phatnhse.sample_food_truck_jc.utils.PreviewSurface
 import com.phatnhse.sample_food_truck_jc.utils.SingleDevicePreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 @Composable
 fun TabLayout(
@@ -32,79 +47,102 @@ fun TabLayout(
 ) {
     var selectedTabIndex by remember { mutableStateOf(initialPage) }
     val tabBackground = colorScheme.onBackgroundSecondary(opacity = 0.2F)
-    val tabBackgroundSelected = colorScheme.surface
-    var tabPositionss by remember {
+    val tabBackgroundSelected = colorScheme.secondaryContainer
+    var tabPositions by remember {
         mutableStateOf<List<TabPosition>>(listOf())
     }
 
-//    val transition = updateTransition(selectedTabIndex, label = "")
-//    val indicatorStart by transition.animateDp(
-//        transitionSpec = {
-//            if (initialState < targetState) {
-//                spring(dampingRatio = 1f, stiffness = 50f)
-//            } else {
-//                spring(dampingRatio = 1f, stiffness = 1000f)
-//            }
-//        }, label = ""
-//    ) {
-//        tabPositions[it].left
-//    }
-//
-//    val indicatorEnd by transition.animateDp(
-//        transitionSpec = {
-//            if (initialState < targetState) {
-//                spring(dampingRatio = 1f, stiffness = 1000f)
-//            } else {
-//                spring(dampingRatio = 1f, stiffness = 50f)
-//            }
-//        }, label = ""
-//    ) {
-//        tabPositions[it].right
-//    }
+    val transition = updateTransition(selectedTabIndex, label = "")
 
-//    TabRowDefaults.Indicator(
-//        Modifier
-//            .fillMaxSize()
-//            .wrapContentSize(align = Alignment.BottomStart)
-//            .offset(x = indicatorStart)
-//            .width(indicatorEnd - indicatorStart),
-//        color = tabBackgroundSelected,
-//    )
+    val indicatorStart by transition.animateDp(
+        transitionSpec = {
+            spring(
+                dampingRatio = 0.8F,
+                stiffness = Spring.StiffnessMedium
+            )
+        }, label = ""
+    ) {
+        if (tabPositions.isEmpty()) {
+            0.dp
+        } else {
+            tabPositions[it].left
+        }
+    }
+
+    val indicatorEnd by transition.animateDp(
+        transitionSpec = {
+            spring(
+                dampingRatio = 0.8F,
+                stiffness = Spring.StiffnessMedium
+            )
+        }, label = ""
+    ) {
+        if (tabPositions.isEmpty()) {
+            0.dp
+        } else {
+            tabPositions[it].right
+        }
+    }
 
     Column(modifier) {
-        TabRow(
-            selectedTabIndex = selectedTabIndex,
-            containerColor = tabBackground,
-            modifier = Modifier
+        Box(
+            Modifier
+                .height(IntrinsicSize.Max)
                 .padding(PaddingNormal)
-                .clip(RoundedLarge),
-            indicator = @Composable { tabPositions ->
-                tabPositionss = tabPositions
-            },
-            divider = {}
         ) {
-            tabItems.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = {
-                        selectedTabIndex = index
-                        onTabSelected(index)
-                    },
-                    text = {
-                        Text(
-                            text = title,
-                            style = typography.titleSmall.copy(
-                                color = colorScheme.onBackground
+            Box(
+                modifier = Modifier
+                    .offset(x = indicatorStart)
+                    .width(indicatorEnd - indicatorStart)
+                    .fillMaxHeight()
+                    .padding(PaddingSmall)
+                    .clip(RoundedLarge)
+                    .background(color = tabBackgroundSelected)
+            )
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                containerColor = tabBackground,
+                modifier = Modifier
+                    .height(48.dp)
+                    .clip(RoundedLarge),
+                indicator = @Composable { positions ->
+                    tabPositions = positions
+                },
+                divider = {}
+            ) {
+                tabItems.forEachIndexed { index, title ->
+                    Tab(
+                        interactionSource = remember { NoRippleInteractionSource() },
+                        selected = selectedTabIndex == index,
+                        onClick = {
+                            selectedTabIndex = index
+                            onTabSelected(index)
+                        },
+                        text = {
+                            Text(
+                                text = title, style = typography.titleSmall.copy(
+                                    color = colorScheme.onBackground
+                                )
                             )
-                        )
-                    },
-                )
+                        },
+                    )
+                }
             }
         }
 
-        tabContent()
+        Box(modifier = Modifier.padding(PaddingNormal)){
+            tabContent()
+        }
     }
+}
 
+class NoRippleInteractionSource : MutableInteractionSource {
+
+    override val interactions: Flow<Interaction> = emptyFlow()
+
+    override suspend fun emit(interaction: Interaction) {}
+
+    override fun tryEmit(interaction: Interaction) = true
 }
 
 @SingleDevicePreview
@@ -114,14 +152,10 @@ fun Tab_Preview() {
         val tabs = listOf("Tab 1", "Tab 2", "Tab 3", "Tab 4")
         var index by remember { mutableStateOf(0) }
 
-        TabLayout(Modifier.fillMaxSize(),
-            tabItems = tabs,
-            tabContent = {
-                Text(text = "tab selected ${tabs[index]}")
-            },
-            onTabSelected = {
-                index = it
-            }
-        )
+        TabLayout(Modifier.fillMaxSize(), tabItems = tabs, tabContent = {
+            Text(text = "tab selected ${tabs[index]}")
+        }, onTabSelected = {
+            index = it
+        })
     }
 }
